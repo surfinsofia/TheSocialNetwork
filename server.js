603b6c325336e53e879b27fa4a9a66f27385bd33
Sofia <asofiae96@gmail.com>
2022-08-29 13:25:53 -0500

    server js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+
+//require express, connection, routes
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
+//create port for localhost and wake up express for use
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//instruct app to use express and routes with specifics
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+//print to console once db connects
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
