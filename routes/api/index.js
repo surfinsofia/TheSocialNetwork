e1875da06639cb3a0abd20d341b774b814a964ae
Sofia <asofiae96@gmail.com>
2022-08-29 13:23:18 -0500

    organizing routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,16 @@
+
+//require express for router
 const router = require('express').Router();
+
+//require userRoutes file
 const userRoutes = require('./userRoutes');
+
+//require thoughtRoutes file
 const thoughtRoutes = require('./thoughtRoutes');
 
+//Intruct router to use these routes
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
+//export connection from this file
 module.exports = router;
